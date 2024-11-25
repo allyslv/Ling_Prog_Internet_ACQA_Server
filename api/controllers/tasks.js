dcc03328da255588fb6a521fbae841c9158e6160
Allysson Ferreira <allyssonferreiraslv@gmail.com>
2024-11-24 21:34:53 -0300

    Initial commit

---
+++ api/controllers/tasks.js
@@ -0,0 +1,27 @@
+let tasks = [];
+
+module.exports = {
+  getAll(req, res) {
+    console.log(req);
+    res.json(tasks);
+  },
+  create(req, res) {
+    const { task } = req.body;
+    console.log(res.body);
+    const newTask = { id: tasks.length + 1, task, done: false };
+    tasks.push(newTask);
+    res.status(201).json(newTask);
+  },
+  markAsDone(req, res) {
+    const { id } = req.params;
+    tasks = tasks.map((task) =>
+      task.id == id ? { ...task, done: true } : task
+    );
+    res.status(204).end();
+  },
+  removeTask(req, res) {
+    const { id } = req.params;
+    tasks = tasks.filter((task) => task.id != id);
+    res.status(204).json(tasks);
+  },
+};
